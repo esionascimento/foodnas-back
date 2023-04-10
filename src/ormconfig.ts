cdcb233b0d5438697006c8a952b5faf1c3093d31
esionascimento <esionascimento@proton.me>
2023-04-09 21:06:18 -0400

    refactor: ormconfig.ts migartions

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,32 +1,37 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import 'dotenv/config';
+import entities from './entities';
+const path = require('path');
 
 export const dataSourceOptions: DataSourceOptions = {
-  url: `mysql://v3uc99nseljiazto9ep6:pscale_pw_uEFghr3LRYD0OeO3m5WymDQPHuDHNqpKciRyfCczbkP@aws.connect.psdb.cloud/foodnas?ssl={"rejectUnauthorized":true}`,
+  url: process.env.DATABASE_URL,
   type: 'mysql',
   // host: process.env.DATABASE_HOST,
   // port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
   // username: process.env.DATABASE_USERNAME,
   // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_DATABASE,
-  entities: ['C:\\Users\\esion\\gitHub\\foodnas-back\\src\\entities\\comida.entity.ts'],
-  migrations: ['dist/database/migrations/*{.ts,.js}'],
-  synchronize: true,
+  database: process.env.DATABASE_DATABASE,
+  entities: entities,
+  migrations: [
+    path.resolve(__dirname, 'src', 'database', 'migrations', '*{.ts,.js}'),
+  ],
+  synchronize: false,
   logging: true,
-  "extra": {
-    "ssl": {
-      "rejectUnauthorized": false
-    }
-  }
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
 const dataSource = new DataSource(dataSourceOptions);
 
-dataSource.initialize()
-.then(() => {
-  console.log("Data Source has been initialized!");
-})
-.catch((err) => {
-  console.error("Error during Data Source initialization", err);
-});
+dataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization');
+  });
 
 export default dataSource;
